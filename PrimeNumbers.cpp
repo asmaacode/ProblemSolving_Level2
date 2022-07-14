24c1a60b3b22187ba25ba724b3c2b912f6109e6d
Asma'a <78433466+asmaacode@users.noreply.github.com>
2022-07-14 19:21:36 +0300

    Update PrimeNumbers.cpp

--- PrimeNumbers.cpp
+++ PrimeNumbers.cpp
@@ -1,25 +1,7 @@
+#include <iostream>
+#include "myLibrary.h"
+using namespace std;
 
-int readNumber() {
-	int userNumber = 0;
-	while (!(cin >> userNumber))
-	{
-		// Explain error
-		cout << "ERROR: A Number must be entered:\n";
-		cin.clear();
-		cin.ignore(132, '\n');
-	}
-	return  userNumber;
-}
-
-int readPositiveNumber() {
-	int positiveNumber = 0;
-	do {
-		cout << "Please enter a number greater than zero :\n";
-		positiveNumber = readNumber();
-	} while (positiveNumber <= 0);
-
-	return positiveNumber;
-}
 bool isPrime(int n) {
 	if (n == 1) return true;
 	int sqRoot = ceil(sqrt(n));
@@ -35,7 +17,7 @@ void printPrimesInRange(int from, int to) {
 }
 
 void main(){
-  int from = readPositiveNumber();
+	int from = readPositiveNumber();
 	int to = readPositiveNumber();
 	printPrimesInRange(from, to);
 }
